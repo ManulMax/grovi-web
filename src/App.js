8bcec8bff8e745468a9f4b59f59e03b2e644b6dd
ManulMax <manulmadara9725@gmail.com>
2021-09-11 21:38:12 +0530

    dashboard added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
-import Sidenav from './components/Dashboard/Sidenav';
+import Dashboard from './components/Dashboard/Dashboard';
 
 function App() {
   return (
     <>
     {/* <Home/> */}
     {/* <Login/> */}
-    <Sidenav/>
+    <Dashboard/>
     </>
   );
 }
